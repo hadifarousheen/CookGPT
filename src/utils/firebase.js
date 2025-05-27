a39591d3775f04dba682a1dedcaa38a13b757655
Hadifa Rousheen <hadifarousheen2644@gmail.com>
2025-05-27 11:29:34 +0530

    change in firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXZ7XH0EWzsxugK3Z_uiHNR6Uy07QXRQA",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "cookgpt-d677a.firebaseapp.com",
   projectId: "cookgpt-d677a",
   storageBucket: "cookgpt-d677a.firebasestorage.app",
